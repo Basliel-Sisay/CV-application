0a0f38ff6bf7ff628e8c72d8f84246922ad40d49
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-11-12 21:03:27 +0300

    Update App.jsx with structural and functional improvements

--- cv-application/src/App.jsx
+++ cv-application/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import General from './src/component/general.jsx';
+import Education from './src/component/eduaction.jsx';
+import Practical from './src/component/practical.jsx';
+import './src/style/style.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [general, setgeneral] = useState({name:'', email:'', phone:''});
+  const [ed, seted] = useState({SchoolName:'', titleOfStudy:'',dateOfStudy:''});
+  const [pr, setpr] = useState({companyName:'',positionTitle:'',responsibilities:'',dateFrom:'',dateUntil:''});
 
   return (
-    <>
-      
-
-    </>
+    <div className="cv-container">
+      <General general={general} setgeneral={setgeneral}/>
+      <Education ed={ed} seted={seted}/>
+      <Practical pr={pr} setpr={setpr}/>
+    </div>
   )
 }
 
-export default App
+export default App;
